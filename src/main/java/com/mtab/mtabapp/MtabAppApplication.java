c8cb63013b7a7f520e16caa1ae79211f13b5e4d6
jarzeckil <jarzeckil@gmail.com>
2025-12-06 22:37:55 +0100

    init project

---
+++ src/main/java/com/mtab/mtabapp/MtabAppApplication.java
@@ -0,0 +1,13 @@
+package com.mtab.mtabapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtabAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MtabAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mtab/mtabapp/MtabAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtab.mtabapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtabAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
