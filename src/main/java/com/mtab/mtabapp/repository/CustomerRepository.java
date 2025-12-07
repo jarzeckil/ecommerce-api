bfddb9f2bbfaccf93a8a3ce1d490eb33c977e53f
jarzeckil <jarzeckil@gmail.com>
2025-12-07 15:40:00 +0100

    changed return type to optional

--- src/main/java/com/mtab/mtabapp/repository/CustomerRepository.java
+++ src/main/java/com/mtab/mtabapp/repository/CustomerRepository.java
@@ -3,6 +3,8 @@ package com.mtab.mtabapp.repository;
 import com.mtab.mtabapp.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    Customer findByEmailAddress(String emailAddress);
+    Optional<Customer> findByEmailAddress(String emailAddress);
 }
