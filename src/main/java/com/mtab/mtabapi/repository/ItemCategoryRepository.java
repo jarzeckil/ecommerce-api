8c698743c52ef10c0bc29d32b5b3112b1ed68800
jarzeckil <jarzeckil@gmail.com>
2025-12-07 17:49:20 +0100

    added additional finding methods

--- src/main/java/com/mtab/mtabapi/repository/ItemCategoryRepository.java
+++ src/main/java/com/mtab/mtabapi/repository/ItemCategoryRepository.java
@@ -3,5 +3,8 @@ package com.mtab.mtabapi.repository;
 import com.mtab.mtabapi.entity.ItemCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ItemCategoryRepository extends JpaRepository<ItemCategory, Long> {
+    Optional<ItemCategory> findByName(String name);
 }
--- src/main/java/com/mtab/mtabapi/repository/ItemRepository.java
+++ src/main/java/com/mtab/mtabapi/repository/ItemRepository.java
@@ -1,10 +1,13 @@
 package com.mtab.mtabapi.repository;
 
 import com.mtab.mtabapi.entity.Item;
+import com.mtab.mtabapi.entity.ItemCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findByCategoryId(Long category_id);
+    Optional<Item> findByNameAndCategory(String name, ItemCategory category);
 }
